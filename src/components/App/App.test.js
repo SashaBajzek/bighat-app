cba8f83e1f29de2555b23bb15a45627bb55197b6
Sasha Bajzek <sasha.bajzek@gmail.com>
2024-02-15 18:02:04 -0800

    Add unit test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("runs the first test case", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const runButton = screen.getByText("Use Test Case 1");
+
+  userEvent.click(runButton);
+
+  await waitFor(() => {
+    expect(screen.getByText("A")).toBeInTheDocument();
+  });
+  await waitFor(() => {
+    expect(screen.getByText("Elapsed Time: 0s")).toBeInTheDocument();
+  });
 });
